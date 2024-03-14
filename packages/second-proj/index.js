206fbaf2262f222e1a28217549f35106a2171f8c
volodymyr.boichuk <volodymyr.boichuk@softconstruct.com>
2024-03-14 02:22:47 +0200

    chore(release): publish 0.2.10

--- packages/second-proj/index.js
+++ packages/second-proj/index.js
@@ -4,3 +4,4 @@ console.log('second project feature_V2');
 console.log('second project feature_V3');
 console.log('second project feature_V4');
 console.log('second project feature_V5');
+console.log('second project feature_V6');
